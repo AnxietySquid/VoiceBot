8e53f6e40f2dc4108c4622c4ed92be9fc1a67d98
Sergey <anxietysquid@yandex.ru>
2025-10-01 15:55:51 +0700

    added text2voice, only in english for now

--- backend/bot.py
+++ backend/bot.py
@@ -1,7 +1,15 @@
 from typing import Final
 import os
 from telegram import Update
-from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+from telegram.ext import (
+    Application, 
+    CommandHandler, 
+    MessageHandler, 
+    ConversationHandler,
+    filters, 
+    ContextTypes
+)
+
 from dotenv import load_dotenv
 
 from tts_utils import text_to_speech
@@ -12,21 +20,30 @@ TOKEN: Final = os.getenv("BOT_TOKEN")
 BOT_USERNAME: Final = '@auto_voice_bot'
 VOICE_MESSAGES_PATH: Final = "./voices"
 DEFAULT_USER_VOICE_DURATION: Final = 60
+# Tracks the state whether bot is waiting for text
+WAITING_FOR_TEXT = 1
 
 os.makedirs(VOICE_MESSAGES_PATH, exist_ok=True)
 
 # Commands
 
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # Debug
+    print(f'Command "start" has been invoked by user "{update.message.chat.username}"' )
     await update.message.reply_text("Hello! Send me a voice message and a text right after!")
 
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # Debug
+    print(f'Command "help" has been invoked by user "{update.message.chat.username}"' )
     await update.message.reply_text("Here are the instructions")
 
 
-async def custom_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Here is the custom command")
+async def tts_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # Debug
+    print(f'Command "tts" has been invoked by user "{update.message.chat.username}"' )
+    await update.message.reply_text("Send me the text and I will voice it over!")
+    context.user_data["awaiting_tts"] = True
 
 # Responses
 # Process the text that the user sents
@@ -42,17 +59,29 @@ def handle_response(text: str) -> tuple[str, str]:
         return ('text', 'fine')
 
     if 'music' in processed:
-        return ('voice', 'example.mp3')
+        return ('voice', 'output.mp3')
 
     return ('text', 'I do not understand you')
 
 
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    '''
+    Handles user messages 
+    '''
     message_type: str = update.message.chat.type
     text: str = update.message.text
     # Debug
     print(f'User {update.message.chat.username} ({update.message.chat.id}) in {message_type} said: "{text}"')
+
+    if context.user_data.get("awaiting_tts"):
+        context.user_data["awaiting_tts"] = False  # reset flag
+
+        file_path = text_to_speech(text, f"{update.message.chat.id}_tts.mp3")
+        with open(file_path, "rb") as voice_file:
+            await update.message.reply_voice(voice=voice_file, caption="Here is your audio")
+            print(f'The voice message {update.message.chat.id}_tts.mp3 was sent to {update.message.chat.username}')
+        return
 
     if message_type == 'group':
         if BOT_USERNAME in text:
@@ -68,11 +97,14 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await update.message.reply_text(response_value)
     elif response_type == 'voice':
         with open(response_value, "rb") as voice_file:
-            await update.message.reply_voice(voice=voice_file, caption="Here you go")
-
+            await update.message.reply_voice(voice=voice_file, caption="Here is the result")
+    # Debug
     print(f'Bot responded with {response_type}: {response_value}')
 
 async def handle_voice(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    '''
+    Handles user voice messages
+    '''
     voice = update.message.voice
     file_id = voice.file_id
     duration = voice.duration
@@ -90,6 +122,7 @@ async def handle_voice(update: Update, context: ContextTypes.DEFAULT_TYPE):
         print(f"Voice message from {update.message.chat.username} will not be saved (id = {voice.file_unique_id}), too long: {duration}s > {DEFAULT_USER_VOICE_DURATION}s") 
         await update.message.reply_text(f"Voice message is too long, make it {DEFAULT_USER_VOICE_DURATION}s or less")
 
+
 # Debug
 async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
     print(f'Update {update} caused error {context.error}')
@@ -104,7 +137,7 @@ if __name__ == '__main__':
     # Commands
     app.add_handler(CommandHandler('start', start_command))
     app.add_handler(CommandHandler('help', help_command))
-    app.add_handler(CommandHandler('custom', custom_command))
+    app.add_handler(CommandHandler('tts', tts_command))
 
 
     # Messages
@@ -118,15 +151,5 @@ if __name__ == '__main__':
     # Debug
     print('Polling...')
     app.run_polling(poll_interval=3)
-
-
-
-
-
-
-
-
-
-
 
 
