81cc23bdfba30b3d578dd50fd65a5d8de85c1ac8
Sergey <anxietysquid@yandex.ru>
2025-10-16 11:22:04 +0700

    Changed the way encoder, sythesizer and vocoder are downloaded

--- utils/default_models.py
+++ utils/default_models.py
@@ -1,56 +1,52 @@
-import urllib.request
 from pathlib import Path
-from threading import Thread
-from urllib.error import HTTPError
-
-from tqdm import tqdm
-
+import hashlib
+import gdown
 
 default_models = {
-    "encoder": ("https://drive.google.com/uc?export=download&id=1Fahntl4uiuOLZzkgyUR8NhtMIKys6BNY", 17090379),
-    "synthesizer": ("https://drive.google.com/file/d/1TGN7QqixdYnov5o4SUT6yr5xH5xw73JY/view?usp=sharing", 370554559),
-    "vocoder": ("https://drive.google.com/file/d/19BSNQ7f5fSB-AxzPcZS5KbAUG61W79rj/view?usp=sharing", 53845290),
+    "encoder": {
+        "id": "1Fahntl4uiuOLZzkgyUR8NhtMIKys6BNY",
+        "sha256": "39373b86598fa3da9fcddee6142382efe09777e8d37dc9c0561f41f0070f134e"},
+    "synthesizer": {
+        "id": "1TGN7QqixdYnov5o4SUT6yr5xH5xw73JY",
+        "sha256": "c05e07428f95d0ed8755e1ef54cc8ae251300413d94ce5867a56afe39c499d94"},
+    "vocoder": {
+        "id": "19BSNQ7f5fSB-AxzPcZS5KbAUG61W79rj",
+        "sha256": "1d7a6861589e927e0fbdaa5849ca022258fe2b58a20cc7bfb8fb598ccf936169"},
 }
 
+def compute_sha256(path: Path) -> str:
+    h = hashlib.sha256()
+    with path.open("rb") as f:
+        for chunk in iter(lambda: f.read(8192), b""): # Loads with chunks 8192 bytes each until returns ""
+            h.update(chunk)
+    return h.hexdigest()
 
-class DownloadProgressBar(tqdm):
-    def update_to(self, b=1, bsize=1, tsize=None):
-        if tsize is not None:
-            self.total = tsize
-        self.update(b * bsize - self.n)
 
 
-def download(url: str, target: Path, bar_pos=0):
+def download(file_id: str, target: Path):
+    url = f"https://drive.google.com/uc?id={file_id}"
     # Ensure the directory exists
     target.parent.mkdir(exist_ok=True, parents=True)
-
     desc = f"Downloading {target.name}"
-    with DownloadProgressBar(unit="B", unit_scale=True, miniters=1, desc=desc, position=bar_pos, leave=False) as t:
-        try:
-            urllib.request.urlretrieve(url, filename=target, reporthook=t.update_to)
-        except HTTPError:
-            return
-
+    gdown.download(url, str(target), quiet=False)
 
 def ensure_default_models(models_dir: Path):
     # Define download tasks
     jobs = []
-    for model_name, (url, size) in default_models.items():
+    for model_name, info in default_models.items():
         target_path = models_dir / "default" / f"{model_name}.pt"
-        if target_path.exists():
-            if target_path.stat().st_size != size:
-                print(f"File {target_path} is not of expected size, redownloading...")
-            else:
-                continue
+        expected_hash = info["sha256"]
+        needs_download = True
 
-        thread = Thread(target=download, args=(url, target_path, len(jobs)))
-        thread.start()
-        jobs.append((thread, target_path, size))
+        while needs_download:
 
-    # Run and join threads
-    for thread, target_path, size in jobs:
-        thread.join()
+            download(info["id"], target_path)
 
-        assert target_path.exists() and target_path.stat().st_size == size, \
-            f"Download for {target_path.name} failed. You may download models manually instead.\n" \
-            f"https://drive.google.com/drive/folders/1fU6umc5uQAVR2udZdHX-lDgXYzTyqG_j"
+            if target_path.exists():
+                actual_hash = compute_sha256(target_path)
+                if expected_hash == actual_hash:
+                    print(f"{model_name} downloaded and verified!")
+                    needs_download = False
+                else:
+                    print(f"{model_name} has hash mismatch, redownloading...")
+
